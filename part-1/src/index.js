e8182ff755977bca1ab087d62a4a200d84699168
Chris Pearce <hello@chrispearce.me>
2017-03-02 14:22:04 +0000

    Bootstrap part-1

---
+++ part-1/src/index.js
@@ -0,0 +1,16 @@
+// the only two imports you'd need to build an app...
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// add some global styles to the page
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import './index.css';
+
+// TODO: Write your application
+
+// render/mount our application into the page
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
